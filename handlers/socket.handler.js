08872073521128523f86eba2c4b6a51a2aa6af2a
Shahrukh <shahrukhmuhammad65@gmail.com>
2024-01-11 00:24:07 +0500

    socket implementation

--- handlers/socket.handler.js
+++ handlers/socket.handler.js
@@ -12,10 +12,49 @@ export default class socketHandler {
             return this.#io
         } else {
             const server = http.createServer(app)
+            server.listen(3001, () => {
+                console.log(`Socket is listening to http://localhost:3001`);
+            })
 
-            this.#io = new Server(server, { cors: { origin: 'https://a771-2407-d000-d-c9a9-9888-88b4-b4d0-5b52.ngrok.io', methods: ['GET', 'POST'] } })
-            console.log(`Socket is listening to http://localhost:8081`)
+            this.#io = new Server(server, {
+                cors: {
+                    origin: 'http://ec2-44-205-32-103.compute-1.amazonaws.com:8080',
+                    methods: ['GET', 'POST']
+                }
+            });
+
+            this.#io.on('connection', (socket) => {
+                console.log('A user connected');
+
+                // Identify the user as vendor or customer
+                
+                socket.on('identify', (role, customerId) => {
+                    socket.role = role;
+                    socket.customerId = customerId;
+                    console.log(`A ${role} connected with customerId ${customerId}`);
+                    
+                    // Join the room based on customerId
+                    socket.join(`customer_${customerId}`);
+                  });
+
+                // Handle coordinates from vendors and broadcast to customers
+                socket.on('coordinates', (data) => {
+                    console.log(data);
+
+                    // Broadcast coordinates to customers
+                   socket.to(`customer_${socket.customerId}`).emit('vendorCoordinates', data);
+                });
+
+                // Handle disconnection
+                socket.on('disconnect', () => {
+                    console.log(`A ${socket.role} disconnected`);
+                });
+            });
+
         }
+
+
+
     }
 
 }
