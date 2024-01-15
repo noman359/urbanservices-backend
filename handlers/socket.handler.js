b9694db8dc256e46ad5d84ab0f705c4a16887722
Shahrukh <shahrukhmuhammad65@gmail.com>
2024-01-15 13:47:48 +0500

    socket implementation

--- handlers/socket.handler.js
+++ handlers/socket.handler.js
@@ -11,6 +11,8 @@ export default class socketHandler {
         if (Object.keys(this.#io).length > 0) {
             return this.#io
         } else {
+            // const  customer = Customers()
+            // const vendor = Vendor()
             const server = http.createServer(app)
             server.listen(8081,() => {
                 console.log(`Socket is listening to http://localhost:8081`);
@@ -25,24 +27,29 @@ export default class socketHandler {
 
             this.#io.on('connection', (socket) => {
                 console.log('A user connected');
-
+                console.log(socket)
                 // Identify the user as vendor or customer
                 
                 socket.on('identify', (role, customerId) => {
                     socket.role = role;
                     socket.customerId = customerId;
                     console.log(`A ${role} connected with customerId ${customerId}`);
-                    
+                    console.log(`customer_${customerId}`);
                     // Join the room based on customerId
-                    socket.join(`customer_${customerId}`);
+                    
+                 socket.join(`customer_${customerId}`);
                   });
 
                 // Handle coordinates from vendors and broadcast to customers
                 socket.on('coordinates', (data) => {
-                    console.log(data);
+
+                    console.log(`coordinates channel received the data at customer_${socket.customerId}`);
+                    console.log('Data is: ',data);
 
                     // Broadcast coordinates to customers
-                   socket.to(`customer_${socket.customerId}`).emit('vendorCoordinates', data);
+                socket.broadcast.to(`customer_${socket.customerId}`).emit('vendorCoordinates', data);
+
+                 // socket.to(`customer_${socket.customerId}`).emit('vendorCoordinates', data);
                 });
 
                 // Handle disconnection
